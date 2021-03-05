3f91ae3cbf5dc7f53fcb3afae5f54851520434a5
keento0809 <handball0321@gmail.com>
2021-03-05 22:58:34 +0900

    Fix search link

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -4,15 +4,14 @@
 "use strict";
 {
   // アカウント作成画面のパスワードと確認用の一致をチェック
-  // const createBtn = document.getElementById('js-create');
-  // createBtn.addEventListener('click',function(e){
-  //   let password = document.getElementById('js-password');
-  //   let confirmPassword = document.getElementById('js-confirmPassword');
-  //   if(password.value !== confirmPassword.value){
+  // const createBtn = document.getElementById("js-create");
+  // createBtn.addEventListener("click", function (e) {
+  //   let password = document.getElementById("js-password");
+  //   let confirmPassword = document.getElementById("js-confirmPassword");
+  //   if (password.value !== confirmPassword.value) {
   //     e.preventDefault();
-  //     alert('パスワードと確認用パスワードが一致しません。')
+  //     alert("パスワードと確認用パスワードが一致しません。");
   //   }
-
   // });
 
   // 表示する画像を配列で持つ
